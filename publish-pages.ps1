1f6734f8b3ccc4057416650a6f4d826ff51c0f91
DYNASOFT-HP\amladjo <amladjo@gmail.com>
2026-02-03 20:35:58 +0100

    smanji padding datum linije

--- publish-pages.ps1
+++ publish-pages.ps1
@@ -3,7 +3,14 @@ $ErrorActionPreference = "Stop"
 $project = "src/SmBlazor/SmBlazor.csproj"
 $out = "docs"
 
+# clean output folder
+if (Test-Path $out) {
+    Write-Host "Removing existing output folder: $out"
+    Remove-Item -Path $out -Recurse -Force
+}
+
 # publish
 dotnet publish $project -c Release -o $out
 
 Write-Host "Blazor WASM published for GitHub Pages"
+
